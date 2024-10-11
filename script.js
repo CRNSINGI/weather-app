ed304acb2e76d0f3ad7b26e8ad8921b9105b245b
Cesar <cesarnsingi@hotmail.com>
2024-10-12 03:52:40 +0400

    get weatherapi

--- script.js
+++ script.js
@@ -4,3 +4,7 @@ const weatherBox = document.querySelector('.weather-box');
 const weatherDetails = document.querySelector('.weather-details');
 
 
+search.addEventListener('click', () => {
+
+     const APIKey = '24e1fbb280bbd63e3bd0bb11a9576d54';
+});
