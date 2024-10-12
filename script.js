29736407c3bde05a87cb54514c3766821a658474
Cesar <cesarnsingi@hotmail.com>
2024-10-12 04:45:37 +0400

    api update with current weather

--- script.js
+++ script.js
@@ -55,6 +55,11 @@ search.addEventListener('click', () => {
                     image.src= 'images/cloud.png';
          }
 
+        temperature.innerHTML = `${parseInt(json.main.temp)}<span>°C</span>`;
+        description.innerHTML = `${json.weather[0].description}`;
+        humidity.innerHTML = `${json.main.humidity}%`;
+        wind.innerHTML = `${parseInt(json.wind.speed)}Km/h`;
+
 
      });
 
