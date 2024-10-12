80b6b58ff76f9c22010e6c00ec287ead126b40dc
Cesar <cesarnsingi@hotmail.com>
2024-10-12 04:05:41 +0400

    js fix api response

--- script.js
+++ script.js
@@ -7,4 +7,16 @@ const weatherDetails = document.querySelector('.weather-details');
 search.addEventListener('click', () => {
 
      const APIKey = '24e1fbb280bbd63e3bd0bb11a9576d54';
+     const city = document.querySelector('.search-box input').value;
+
+     if (city == '')
+        return;
+
+     fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${APIKey}`).then(response => response.json()).then(json => { 
+
+         const image = document.querySelector('.weather-box img');
+
+     });
+
+
 });
